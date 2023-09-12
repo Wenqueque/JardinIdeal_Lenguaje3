623df040c4dc7c2aa35be734b3bff1dfba475502
Wenqueque <eleasamekichi@gmail.com>
2023-09-12 04:15:46 -0300

    Nivel 3 Listo (Faltan malezas)

--- Assets/script/ObjectsManagerLlamado.cs
+++ Assets/script/ObjectsManagerLlamado.cs
@@ -18,6 +18,7 @@ public class ObjectsManagerLlamado : MonoBehaviour
         bool seVeAbonar = false;
         bool seVeTijeras = false;
         bool seVeMano = false;
+        bool seVePozo = false;
 
         if (Physics.Raycast(ray, out hit, maxDistance, layerMask))
         {
@@ -34,6 +35,20 @@ public class ObjectsManagerLlamado : MonoBehaviour
                 //Debug.Log("MOSTRANDO ABONO");
                 seVeAbonar = true;
             }
+
+            // Si el rayo golpea un objeto con la etiqueta "Abonar", establecemos seVeAbonar a true.
+            if (hit.collider.CompareTag("Fruta"))
+            {
+                //Debug.Log("MOSTRANDO ABONO");
+                seVeMano = true;
+            }
+
+            // Si el rayo golpea un objeto con la etiqueta "Abonar", establecemos seVeAbonar a true.
+            if (hit.collider.CompareTag("Semilla"))
+            {
+                //Debug.Log("MOSTRANDO ABONO");
+                seVePozo = true;
+            }
         }
 
         // Configuramos el estado activo de los GameObjects en la lista en función de si se ven los objetos con las etiquetas "Fuente" y "Abonar".
@@ -41,5 +56,6 @@ public class ObjectsManagerLlamado : MonoBehaviour
         objetosAMostrar[1].SetActive(seVeAbonar);
         objetosAMostrar[2].SetActive(seVeTijeras);
         objetosAMostrar[3].SetActive(seVeMano);
+        objetosAMostrar[4].SetActive(seVePozo);
     }
 }
